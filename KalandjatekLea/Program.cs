57213f595ea55a795eee2d65425d3dc673baaba7
Petrik Lajos Szg <info@petrik.hu>
2020-10-06 12:20:12 +0200

    UI elkezdés

--- KalandjatekLea/Program.cs
+++ KalandjatekLea/Program.cs
@@ -10,6 +10,7 @@ namespace KalandjatekLea
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ KalandjatekLea/UI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalandjatekLea
+{
+    class UI
+    {
+        public void KarakterBekeres()
+        {
+            Console.WriteLine("Kérem a karakter nevét");
+            string BekertNev = Console.ReadLine();
+            Console.WriteLine("Kérem a karakter státuszsablonját");
+            int BekertStatuszSablon = Convert.ToInt32(Console.ReadLine());
+            Karakter k1 = new Karakter(BekertNev, BekertStatuszSablon);
+        }
+        public void Kiiras()
+        {
+
+        }
+    }
+}
